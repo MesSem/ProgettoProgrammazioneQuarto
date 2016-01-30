72d1bb68f1aa9372f92708b5994db896f90f8194
morettini <morettini@morettini-System-Product-Name>
2016-01-30 10:12:22 +0100

    Edit of some interfaces

--- Gioco_Quarto/src/interfaces/I_board.java
+++ Gioco_Quarto/src/interfaces/I_board.java
@@ -1,7 +1,15 @@
 package interfaces;
 
+import classes.Piece;
+
 public interface I_board {
 	void loadBoard(String path);
 	void saveBoard(String path);
 	int gameSituation(); //tells whether the match is ended with a victory, a defeat or if it's not ended yet.
+	
+	//utili, poi possiamo anche levarli, ne dobbiamo parlare
+	int size();//get size of board(for example 16 element)
+	boolean isFree(int position);//ask if the position is free. se invio 9, indico la prima cella della terza riga
+	void putPieceAtPosition(Piece piece, int position); //la posizione funziona come sopra
+	Piece removePieceAtPosition(int position);//posizioni gestite sempre allo stesso modo, restituisci indietro l'oggetto e metti null	
 }
--- Gioco_Quarto/src/interfaces/I_table.java
+++ Gioco_Quarto/src/interfaces/I_table.java
@@ -1,8 +1,15 @@
 package interfaces;
 
+import java.util.ArrayList;
+
+import classes.Board;
+import classes.Piece;
+
 public interface I_table {
 	void loadNotUsedPieces(String path);
 	void setEnemyPiece(I_piece pedina); //Sets on a var the chosen pieces.
 	void savePieces(String path); //saves all the left pieces on the file, the chosen piece is the first one at the top.
-
+	Board getACopyOfTheBoard();
+	ArrayList<Piece> getACopyOfPieceNotUsed();
+	Piece getPieceToPosition();
 }
