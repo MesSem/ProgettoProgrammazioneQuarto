fd77d15ac2d51214a17990f147ea3d1ae8fcc039
Enrico <enrico.candelaresi>
2016-01-30 14:29:52 +0100

    non ho modificato nulla, solo alcuni commenti

--- Gioco_Quarto/src/classes/Board.java
+++ Gioco_Quarto/src/classes/Board.java
@@ -3,6 +3,8 @@ package classes;
 import interfaces.I_board;
 
 public class Board implements I_board {
+	
+	//public vettore[16]/matrice[4x4] 
 
 	@Override
 	public void loadBoard(String path) {
@@ -20,6 +22,30 @@ public class Board implements I_board {
 	public int gameSituation() {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isFree(int position) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void putPieceAtPosition(Piece piece, int position) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Piece removePieceAtPosition(int position) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- Gioco_Quarto/src/interfaces/I_board.java
+++ Gioco_Quarto/src/interfaces/I_board.java
@@ -8,7 +8,7 @@ public interface I_board {
 	int gameSituation(); //tells whether the match is ended with a victory, a defeat or if it's not ended yet.
 	
 	//utili, poi possiamo anche levarli, ne dobbiamo parlare
-	int size();//get size of board(for example 16 element)
+	int size();//get size of board(for example 16 elements)
 	boolean isFree(int position);//ask if the position is free. se invio 9, indico la prima cella della terza riga
 	void putPieceAtPosition(Piece piece, int position); //la posizione funziona come sopra
 	Piece removePieceAtPosition(int position);//posizioni gestite sempre allo stesso modo, restituisci indietro l'oggetto e metti null	
