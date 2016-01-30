86c181c0dc6e1222e356426e3e4d8fee6bc34605
Enrico <enrico.candelaresi>
2016-01-30 16:34:48 +0100

    Enum vars in Piece

--- Gioco_Quarto/src/classes/Piece.java
+++ Gioco_Quarto/src/classes/Piece.java
@@ -2,6 +2,21 @@ package classes;
 
 import interfaces.I_piece;
 
-public class Piece implements I_piece {
 
+
+public class Piece implements I_piece {
+	private enum Size{ A, B }; //Tall, Small
+	private enum Colour{ W, N };// White, Black
+	private enum Shape { T, Q }; // Squared, rounded
+	private enum Volume { P, F }; //filled, cave
+	Size size;
+	Colour colour;
+	Shape shape;
+	Volume volume;
+	
+	
+	public Piece(char [] piece){
+		size
+			}
 }
+
