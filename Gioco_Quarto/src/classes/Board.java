57f27cba7d305064d040774ab6c289325a7957df
Enrico <enrico.candelaresi>
2016-02-01 15:29:53 +0100

    added inputs in Board

--- Gioco_Quarto/src/classes/Board.java
+++ Gioco_Quarto/src/classes/Board.java
@@ -1,24 +1,43 @@
 package classes;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
 import interfaces.I_board;
 
 public class Board implements I_board {
 	
-public int [] [] board= new int [4] [4];
+public Piece [] [] board= new Piece [4] [4];
 	@Override
 	public void loadBoard(String path) {
 	try
 	{
-		//creation of variables to read the file. 
-		for(int r=0;r<4;r++)
-		{//read a whole row, split it , fill an array of strings called 4ex: row. ---> Split: String [] app= row.Split(' ');
-		// calls an input-checking method 4ex: boolean okay= checkInput( app )
-		// if it's okay then i can insert values into the board.
-			for(int c=0; c<4;c++)
-			{ //puts the values into the board. 4ex: board[r,c]=app[c];
-			}
+		//creation of variables to read the file.
+		File boardfile=new File(path);
+		if(boardfile.exists())
+		{
+				FileReader br=new FileReader(boardfile);
+				BufferedReader boardReader= new BufferedReader(br);
+				String app;
+				int cont=0;
+				//for(int r=0;r<4;r++)
+				while(((app=boardReader.readLine())!= null) && cont<5 )
+				{//read a whole row, split it , fill an array of strings called 4ex: row. ---> Split: String [] app= row.Split(' ');
+					String[] row=app.split(" ");
+					for(int c=0; c<4;c++)
+					{  // calls an input-checking method 4ex: boolean okay= checkInput( app )
+						// if it's okay then i can insert values into the board.
+						//puts the values into the board. 4ex: board[r,c]=app[c];
+						//board[cont][c]=Piece.CheckAndCreate(row[c]);
+						
+					} 
+				cont++;
+				}
+		}
+		else{
+			//throws exception, file doesn't exist.
 		}
 	}
 	 catch(IOException e){ // ***Commento da cancellare*** ora dà errore ma magari poi creando le variabili di I/O non lo dà più.
