ff8d9b5b210669cdeb7686f814329bb4432c9284
morettini <morettini@morettini-System-Product-Name>
2016-02-16 09:28:15 +0100

    Added comments

--- Gioco_Quarto/src/classes/Player.java
+++ Gioco_Quarto/src/classes/Player.java
@@ -8,8 +8,6 @@ import javax.sql.rowset.serial.SerialArray;
 
 public class Player {
 
-	// tra i controlli bisogna controllare che il gioco non sia già finito
-
 	static Table t;
 	static String pathBoardFile = "/home/morettini/git/ProgettoProgrammazioneQuarto/Pedine & Scacchiera/board.txt";
 	static String pathNotUsedPiece = "/home/morettini/git/ProgettoProgrammazioneQuarto/Pedine & Scacchiera/pieces.txt";
@@ -17,6 +15,7 @@ public class Player {
 										// his piece
 	static int indexBestPieceForEnemy = 0;// index about piece to choose for
 											// enemy
+	static int maxDepth;
 
 	public static void main(String[] args) {
 
@@ -30,34 +29,59 @@ public class Player {
 										// where the algorithm do is
 										// study to choose the better
 										// move
-			ArrayList<Piece> pieceNotUsed = t.getPieceNotUsed();
-			Piece toPosition = t.getPieceToPosition();
-			int result = nextMove(board, pieceNotUsed, toPosition, true, 0, -999, +999);
+			if (t.getGameSituation() == 1000) {
+				ArrayList<Piece> pieceNotUsed = t.getPieceNotUsed();
+				Piece toPosition = t.getPieceToPosition();
+				// If there are a lot of piece to place the algorithm cannot
+				// analyze all the possible combination,
+				// it could employ too much time. So with the switch I set the
+				// maximum attempt;
+				switch (pieceNotUsed.size()) {
+				case 15:
+				case 14:
+				case 13:
+				case 12:
+				case 11:
+					maxDepth = 2;
+					break;
+				case 10:
+				case 9:
+				case 8:
+					maxDepth = 5;
+					break;
+				default:
+					maxDepth = 100;
+					break;
+				}
+				int result = nextMove(board, pieceNotUsed, toPosition, true, 0, -999, +999);
 
-			t.insertPieceInBoardAtPosition(toPosition, indexBestPosition);
+				t.insertPieceInBoardAtPosition(toPosition, indexBestPosition);
 
-			t.setEnemyPiece(pieceNotUsed.get(indexBestPieceForEnemy));
-			t.removePieceNotUsedAtPosition(indexBestPieceForEnemy);
+				t.setEnemyPiece(pieceNotUsed.get(indexBestPieceForEnemy));
+				t.removePieceNotUsedAtPosition(indexBestPieceForEnemy);
 
-			t.savePieces(pathNotUsedPiece);
-			t.saveBoard(pathBoardFile);
+				t.savePieces(pathNotUsedPiece);
+				t.saveBoard(pathBoardFile);
+			} else {
+				throw new BoardConfigurationException("The game is already ended");
+			}
 
 			switch (t.getGameSituation()) {
 			case 0:
-				message="pareggio pos"+indexBestPosition;
+				message = "pareggio pos" + indexBestPosition;
 				messageNumber = 2;
 				break;
 			case 1:
-				message="vinto pos"+ indexBestPosition;
+				message = "vinto pos" + indexBestPosition;
 				messageNumber = 1;
 				break;
 			case 1000:
-				message="continuare pos"+indexBestPosition;
+				message = "continuare pos" + indexBestPosition;
 				messageNumber = 0;
 				break;
-				default:
-					throw new Exception("Error not atteso");
-					
+			default:
+				throw new Exception("Errore non atteso");
+
 			}
 		} catch (IOException e) {
 			message = e.getMessage();
@@ -69,36 +93,52 @@ public class Player {
 			message = e.getMessage();
 			messageNumber = 13;
 		} catch (Exception e) {
+			// Error not expected
 			message = e.getMessage();
-			messageNumber = 999;// errore non atteso
+			messageNumber = 999;
 		}
 
 		System.out.println(message);
-		
-		System.exit(messageNumber);
 
-		// #TODO mettere che si ferma dopo certa profondità POSSIBILE CODICE:
-		// if (depth >10)
-		// if(turno)
-		// return -1;
-		// else
-		// return 1;
-		// forse viceversa.
+		System.exit(messageNumber);
 
 	}
 
+	/**
+	 * This method calculate the best position for the player. It use an
+	 * algorithm similar the Minimax. It is a recursive method that create
+	 * dynamically a tree to explore some possible combinations. It stop after
+	 * some depth because otherwise the execution time will be too long
+	 * 
+	 * @param board
+	 *            the board where the algorithm do his analysis
+	 * @param freePiece
+	 *            list of piece not used
+	 * @param toPosition
+	 *            piece to position
+	 * @param turno
+	 *            it's a boolean variable, if is true, is the Computer turn,
+	 *            otherwise is the turn of the enemy
+	 * @param depth
+	 *            depth of this iteration
+	 * @param alpha
+	 *            maximum result for the player in the father branch
+	 * @param beta
+	 *            minimum result for the enemy in the father branch
+	 * @return the best result possible for the Player
+	 */
 	static int nextMove(Board board, ArrayList<Piece> freePiece, Piece toPosition, Boolean turno, int depth, int alpha,
 			int beta) {
-		if (depth >2)
-			 if(turno)
-			 return +100;
-			 else
-			 return -100;
+		if (depth > maxDepth)
+			if (turno)
+				return +100;
+			else
+				return -100;
 
 		int result = 1000;
 		if (toPosition != null) {
 			for (int i = 0; i < board.size(); i++) {
-				
+
 				if (board.isFree(i)) {
 					board.putPieceAtPosition(toPosition, i);
 
@@ -108,23 +148,23 @@ public class Player {
 							// solo al livello 0, sennò non mi interessa
 							if (result >= alpha && depth == 0) {
 								indexBestPosition = i;
-								if (result == 1){
+								if (result == 1) {
 									board.removePieceAtPosition(i);
 									return result;
-								}// dato che questo ramo da 1,
-													// cioè vittoria non ha
-													// senso andare oltre, mi
-													// salvo l'indice della
-													// posizione e chiudo
+								} // dato che questo ramo da 1,
+									// cioè vittoria non ha
+									// senso andare oltre, mi
+									// salvo l'indice della
+									// posizione e chiudo
 							}
 							alpha = Math.max(alpha, nextMove(board, freePiece, null, turno, depth, alpha, beta));
-							if (beta <= alpha){
+							if (beta <= alpha) {
 								board.removePieceAtPosition(i);
 								break;
 							}
 						} else {
 							beta = Math.min(beta, nextMove(board, freePiece, null, turno, depth, alpha, beta));
-							if (alpha <= beta){
+							if (alpha <= beta) {
 								board.removePieceAtPosition(i);
 								break;
 							}
@@ -135,16 +175,17 @@ public class Player {
 							// solo al livello 0, sennò non mi interessa
 							if (result >= alpha && depth == 0) {
 								indexBestPosition = i;
-								if (result == 1){
+								if (result == 1) {
 									board.removePieceAtPosition(i);
 									return result;
-								}// dato che questo ramo da 1,
-													// cioè vittoria non ha
-													// senso andare oltre, mi
-													// salvo l'indice della
-													// posizione e chiudo
-								// non ha senso testare gli altri rami figli con
-								// il for
+								} // dato che questo ramo da 1,
+									// cioè vittoria non ha
+									// senso andare oltre, mi
+									// salvo l'indice della
+									// posizione e chiudo
+									// non ha senso testare gli altri rami figli
+									// con
+									// il for
 							}
 							alpha = Math.max(alpha, result);
 						} else {
@@ -158,26 +199,26 @@ public class Player {
 			for (int k = 0; k < freePiece.size(); k++) {
 				Piece pieceForEnemy = freePiece.get(k);
 				freePiece.remove(k);
-				result = nextMove(board, freePiece, pieceForEnemy, !turno, depth+1, alpha, beta);
+				result = nextMove(board, freePiece, pieceForEnemy, !turno, depth + 1, alpha, beta);
 
 				if (turno) {
-					if (alpha <=result && depth == 0) {// solo se sono al primo
+					if (alpha <= result && depth == 0) {// solo se sono al primo
 														// livello, sennò non mi
 														// interessa
 						indexBestPieceForEnemy = k;
-						if (result == 1){
+						if (result == 1) {
 							freePiece.add(k, pieceForEnemy);
 							return result;
-							}
+						}
 					}
 					alpha = Math.max(alpha, result);
-					if (beta <= alpha){
+					if (beta <= alpha) {
 						freePiece.add(k, pieceForEnemy);
 						break;
 					}
 				} else {
 					beta = Math.min(beta, result);
-					if (alpha <= beta){
+					if (alpha <= beta) {
 						freePiece.add(k, pieceForEnemy);
 						break;
 					}
@@ -192,11 +233,7 @@ public class Player {
 	}
 
 	/**
-	 * 
-	 * @return 1 vittoria, 0 patta, 1000 casella ancora libera. meglio usare una
-	 *         enum
+	 * @return 1 vittoria, 0 patta, 1000 casella ancora libera
 	 */
-	static int controlla() {
-		return 0;
-	}
+
 }
--- Gioco_Quarto/src/classes/Table.java
+++ Gioco_Quarto/src/classes/Table.java
@@ -7,6 +7,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.EmptyStackException;
 
 import interfaces.I_piece;
 import interfaces.I_table;
@@ -18,7 +19,23 @@ public class Table implements I_table {
 
 	Board board;
 
-	public Table(String pathBoardFile, String pathNotUsedPiecesFile) throws NotUsedPieceConfigurationException, IOException {
+	/**
+	 * Constructor of the Table class
+	 * 
+	 * @param pathBoardFile
+	 *            path of the file that contain the configuration of the Board
+	 * @param pathNotUsedPiecesFile
+	 *            path of the file that contain the list of the piece not used
+	 * @throws NotUsedPieceConfigurationException
+	 *             If there are some mistakes inside the Piece not used file
+	 * @throws BoardConfigurationException
+	 *             If there are some mistakes inside the Board file
+	 * @throws IOException
+	 *             If an input or output exception occurred inside loadBoard or
+	 *             loadNotUsedPiece
+	 */
+	public Table(String pathBoardFile, String pathNotUsedPiecesFile)
+			throws NotUsedPieceConfigurationException, BoardConfigurationException, IOException {
 		// Preparation of the board
 		board = new Board();
 		board.loadBoard(pathBoardFile);
@@ -29,8 +46,6 @@ public class Table implements I_table {
 	}
 
 	/**
-	 * #TODO: It must be checked #TODO: Add check if piece is already in list
-	 * piecenotused
 	 * 
 	 * @param path
 	 * @throws PieceConfigurationException
@@ -39,117 +54,193 @@ public class Table implements I_table {
 	public void loadNotUsedPieces(String path) throws NotUsedPieceConfigurationException, IOException {
 		String line = "";
 		Piece tmp;
+		BufferedReader in = null;
+		FileReader fr;
+		try {
+			fr = new FileReader(path);
 
-		FileReader fr = new FileReader(path);
-
-		BufferedReader in = new BufferedReader(fr);
-		String firstPiece = in.readLine();
-		if (firstPiece != null) {
-			try {
-				tmp = Piece.checkAndCreate(firstPiece);
-			} catch (PieceConfigurationException er) {
-				throw new NotUsedPieceConfigurationException(er.getMessage());
+			in = new BufferedReader(fr);
+			String firstPiece = in.readLine();
+			if (firstPiece != null) {
+				try {
+					tmp = Piece.checkAndCreate(firstPiece);
+				} catch (PieceConfigurationException er) {
+					throw new NotUsedPieceConfigurationException(er.getMessage());
+				}
+				if (!(board.isPlaced(tmp)))
+					pieceToPosition = tmp;
+				else
+					throw new NotUsedPieceConfigurationException("This piece " + line + " is already in the board ");
+			} else
+				throw new NotUsedPieceConfigurationException("No piece on file " + path);
+			while ((line = in.readLine()) != null) {
+				try {
+					tmp = Piece.checkAndCreate(line);
+				} catch (PieceConfigurationException er) {
+					throw new NotUsedPieceConfigurationException(er.getMessage());
+				}
+				if (!board.isPlaced(tmp) && !isPlaced(tmp))
+					pieceNotUsed.add(tmp);
+				else
+					throw new NotUsedPieceConfigurationException("This piece " + line + " is already used ");
 			}
-			if (!(board.isPlaced(tmp)))
-				pieceToPosition = tmp;
-			else
-				throw new NotUsedPieceConfigurationException("This piece " + line + " is already in the board ");
-		} else
-			throw new NotUsedPieceConfigurationException("No piece on file " + path);
-		while ((line = in.readLine()) != null) {
+			in.close();
+		} catch (IOException e) {
 			try {
-				tmp = Piece.checkAndCreate(line);
-			} catch (PieceConfigurationException er) {
-				throw new NotUsedPieceConfigurationException(er.getMessage());
+				in.close();
+			} catch (Exception er) {
+			} finally {
 			}
-			if (!(board.isPlaced(tmp)))
-				pieceNotUsed.add(tmp);
-			else
-				throw new NotUsedPieceConfigurationException("This piece " + line + " is already in the board ");
+			throw e;
 		}
-
-		in.close();
 	}
 
+	/**
+	 * Set the piece to position for the enemy
+	 * 
+	 * @param piece
+	 *            piece to position for the enemy
+	 */
 	@Override
 	public void setEnemyPiece(Piece piece) {
 		pieceToPosition = piece;
 	}
 
 	/**
-	 * #TODO: It must be checked #TODO: Add check if piece is already in list
-	 * piecenotused
+	 * Save the pieces in the file specified by the path
+	 * 
+	 * @param path
+	 *            of the file where you want to save the pieces
 	 * 
 	 * @throws IOException
+	 *             If an I/O error occurs
 	 */
 	@Override
 	public void savePieces(String path) throws IOException {
-		FileWriter fw = new FileWriter(path);
-		BufferedWriter out = new BufferedWriter(fw);
-		out.write(pieceToPosition.toString());
-		String s = pieceToPosition.toString();
-		for (Piece piece : pieceNotUsed) {
-			out.newLine();
-			out.write(piece.toString());
+		BufferedWriter out = null;
+		try {
+			FileWriter fw = new FileWriter(path);
+			out = new BufferedWriter(fw);
+			out.write(pieceToPosition.toString());
+			String s = pieceToPosition.toString();
+			for (Piece piece : pieceNotUsed) {
+				out.newLine();
+				out.write(piece.toString());
+			}
+			out.flush();
+			out.close();
+		} catch (IOException e) {
+			try {
+				out.flush();
+				out.close();
+			} catch (Exception er) {
+			}
+			throw e;
 		}
-		out.flush();
-		out.close();
 	}
 
 	/**
-	 * Better than getBoard() but now not work
+	 * Get the piece choose by the enemy to position by this program
 	 */
-	@Override
-	public Board getACopyOfTheBoard() {
-
-		return null;
-	}
-
-	/**
-	 * Better than getPieceNotUsed() but now not work
-	 */
-	@Override
-	public ArrayList<Piece> getACopyOfPieceNotUsed() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 	@Override
 	public Piece getPieceToPosition() {
 		return pieceToPosition;
 	}
 
+	/**
+	 * Get the board of the Table.
+	 * 
+	 * @return the board as a Board object
+	 */
 	@Override
 	public Board getBoard() {
 		return board;
 	}
 
+	/**
+	 * Get the piece not used of the Table.
+	 * 
+	 * @return the pieceNotUsed as ArrayList of Piece
+	 */
 	@Override
 	public ArrayList<Piece> getPieceNotUsed() {
 		return pieceNotUsed;
 	}
 
+	/**
+	 * Save the board of this table. It use the method inside the Board object
+	 * 
+	 * @param path
+	 *            the path where you want to save the board
+	 * @exception IOException
+	 *                If an input or output exception occurred inside
+	 *                Board.saveBoard(String path);
+	 */
 	@Override
 	public void saveBoard(String path) throws IOException {
 		board.saveBoard(path);
-
 	}
 
+	/**
+	 * Remove a piece in the board in the specified position. The position is
+	 * calculated as the matrix is like a vector.
+	 * 
+	 * @param p
+	 *            piece to position
+	 * @param position
+	 *            index of the position where the method have to remove the
+	 *            piece
+	 */
 	@Override
 	public void removePieceNotUsedAtPosition(int position) {
 		pieceNotUsed.remove(position);
 
 	}
 
+	/**
+	 * Insert a piece in the board in the specified position. The position is
+	 * calculated as the matrix is like a vector.//PER ENRICO, DENTRO BOARD
+	 * METTERE QUALCOSA PER SPIEGARE LA TRASFORMAZIONE DA VETTORE A MATRICE.
+	 * cHIEDI A SIMONE PER CAHIRIMENTI SU COSA INTENDO
+	 * 
+	 * @param p
+	 *            piece to position
+	 * @param position
+	 *            index of the position where the method have to put the piece
+	 */
 	@Override
 	public void insertPieceInBoardAtPosition(Piece p, int position) {
 		board.putPieceAtPosition(p, position);
 
 	}
 
+	/**
+	 * Get the game of the situation in the current instant
+	 * 
+	 * @return the game's situation of the board
+	 */
 	@Override
 	public int getGameSituation() {
 		return board.gameSituation();
+	}
+
+	/**
+	 * Check if a piece is already in pieceNotUsed or is in pieceToPosition
+	 * 
+	 * @param piece
+	 *            that you want to check
+	 * @return true if the piece is already placed, false otherwise
+	 */
+	private boolean isPlaced(Piece p) {
+		if (p.isEqualTo(pieceToPosition)) {
+			return true;
+		} else {
+			for (Piece piece : pieceNotUsed) {
+				if (p.isEqualTo(piece))
+					return true;
+			}
+		}
+		return false;
 	}
 
 }
