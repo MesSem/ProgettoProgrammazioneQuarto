3b16097ee2d75a6a8a04853190ecac7dba21c37b
morettini <morettini@morettini-System-Product-Name>
2016-02-16 17:30:06 +0100

    Deleted comments in interface, now useless

--- Gioco_Quarto/src/interfaces/I_table.java
+++ Gioco_Quarto/src/interfaces/I_table.java
@@ -7,9 +7,9 @@ import classes.Board;
 import classes.Piece;
 
 public interface I_table {
-	void setEnemyPiece(Piece piece); //Sets on a var the chosen pieces.
-	void savePieces(String path) throws IOException; //saves all the left pieces on the file, the chosen piece is the first one at the top.
-	void saveBoard(String path) throws IOException; //saves all the left pieces on the file, the chosen piece is the first one at the top.
+	void setEnemyPiece(Piece piece); 
+	void savePieces(String path) throws IOException; 
+	void saveBoard(String path) throws IOException; 
 	
 	Board getBoard();
 	ArrayList<Piece> getPieceNotUsed();
